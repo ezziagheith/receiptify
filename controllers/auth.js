4f1941345409fb46e64199a3d0a1396a80ff4e52
ezziagheith <ezziagheith@gmail.com>
2019-12-03 15:34:08 -0800

    Register and Login Route

---
+++ controllers/auth.js
@@ -0,0 +1,77 @@
+const bcrypt = require('bcryptjs');
+const db = require('../models');
+
+// POST REGISTER -- CREATE NEW USER
+
+const register = (req, res) => {
+    if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.passwowrd) {
+        return res.status(400).json({status: 400, message: 'Please fill out all the information'});
+    }
+
+    //Verify Account Does Not Already Exist
+    db.User.findOne({ email: req.body.email}, (err, foundUser) => {
+        if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again'});
+
+        if (foundUser) return res.status(400).json({ status: 400, message: 'Email address has already been registered. Please try again'});
+
+        // Generate Salt
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again'});
+            // Hash User Password
+            bcrypt.hash(req.body.password, salt, (err, hash) => {
+                if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again'});
+
+                const newUser = {
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    password: hash,
+                }
+
+                db.User.create(newUser, (err, savedUser) => {
+                    if (err) return res.status(500).json({ status: 500, message: err});
+                    res.status(201).json({
+                        message: 'user created',
+                        user: savedUser
+                    });
+                });
+            });
+        });
+    });
+};
+
+
+// POST Login - Authenticate User, CreateSession
+
+const login = (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({status: 400, message: 'Please enter your email and password'});
+    }
+    db.User.findOne({email: req.body.email}, (err, foundUser) => {
+        if (err) return res.status(500).json({status: 500, message: 'Something went wrong. Please try again'});
+
+        if (!foundUser) {
+            return res.status(400).json({status: 400, message: 'Email or password is incorrect'});
+        }
+        bcrypt.compare(req.body.password, foundUser.password, (err, isMatch) => {
+            if (err) return res.status(500).json({status: 500, message: 'Something went wrong. Please try again'});
+
+            if (isMatch) {
+                req.session.currentUser = {id: foundUser._id};
+                console.log(req.session)
+                return res.status(200).json({ status: 200, message: 'Success', data: foundUser._id});
+            } else {
+                return res.status(400).json({ status: 400, message: 'Email or password is incorrect'});
+            }
+        });
+    });
+};
+
+
+
+
+
+module.exports = {
+    register,
+    login,
+}
